7eae666622fedb374d5d3bd05159980fa75da06e
LKien <128247485+kle12345A@users.noreply.github.com>
2024-02-25 00:25:44 +0700

    LAB1MVC

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson01.MVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson01.MVC.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
